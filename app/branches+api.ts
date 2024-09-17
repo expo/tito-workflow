720f0088e7ef63792ceb2fdb15c46effef63b40a
Brent Vatne <brentvatne@gmail.com>
2024-09-17 14:19:42 -0700

    wip

---
+++ app/branches+api.ts
@@ -0,0 +1,25 @@
+const query = `
+query GetBranches($appId: String!) {
+  app {
+    byId(appId:$appId) {
+      updateBranches(offset:0, limit:10) {
+        name
+      }
+    }
+  }
+}
+`
+
+export async function GET(request: Request) {
+  const response = await fetch('https://api.expo.dev/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.EXPO_TOKEN}`
+    },
+    body: JSON.stringify({ query, variables: { appId: process.env.EXPO_PROJECT_ID } }),
+  });
+
+  const result = await response.json();
+  return Response.json(result);
+}
