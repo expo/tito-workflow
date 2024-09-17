e861af4c9c2de67b8a87c5890cf707acd71a2c35
Brent Vatne <brentvatne@gmail.com>
2024-09-17 14:36:35 -0700

    prettier and other things

--- app/branches+api.ts
+++ app/branches+api.ts
@@ -8,16 +8,19 @@ query GetBranches($appId: String!) {
     }
   }
 }
-`
+`;
 
 export async function GET(request: Request) {
-  const response = await fetch('https://api.expo.dev/graphql', {
-    method: 'POST',
+  const response = await fetch("https://api.expo.dev/graphql", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.EXPO_TOKEN}`
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.EXPO_TOKEN}`,
     },
-    body: JSON.stringify({ query, variables: { appId: process.env.EXPO_PROJECT_ID } }),
+    body: JSON.stringify({
+      query,
+      variables: { appId: process.env.EXPO_PROJECT_ID },
+    }),
   });
 
   const result = await response.json();
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,37 @@
-import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { useEffect, useCallback, useState } from "react";
+import { Button, Text, View } from "react-native";
+import * as Updates from "expo-updates";
+
+type Branch = string;
+
+// Can also point to a local server in dev
+const API_URL = `https://tito-workflow--n1v6uoewca.staging.expo.app/branches`;
 
 export default function Index() {
-   
+  const [branches, setBranches] = useState<Branch[]>([]);
+  const { currentlyRunning } = Updates.useUpdates();
+
+  // TODO: persist the selected branch
+  const selectBranch = useCallback(async (branch: Branch) => {
+    await Updates.setExtraParamAsync("branch", branch);
+    await Updates.fetchUpdateAsync();
+    await Updates.reloadAsync();
+  }, []);
+
+  const fetchBranches = useCallback(async () => {
+    const response = await fetch(API_URL);
+    const result = await response.json();
+    setBranches(
+      result.data.app.byId.updateBranches.map(
+        (branch: { name: string }) => branch.name,
+      ),
+    );
+  }, []);
+
+  useEffect(() => {
+    fetchBranches();
+  }, [fetchBranches]);
+
   return (
     <View
       style={{
@@ -11,7 +40,24 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>
+        Currently running:{" "}
+        {currentlyRunning.updateId ? currentlyRunning.updateId : "no update"} /{" "}
+        {currentlyRunning.createdAt?.toString()}
+      </Text>
+
+      <View style={{ marginTop: 16 }}>
+        <Text style={{ fontWeight: "bold" }}>Branches</Text>
+      </View>
+
+      {branches.map((branch) => (
+        <Button
+          key={branch}
+          onPress={() => selectBranch(branch)}
+          title={branch}
+        />
+      ))}
+      <Button title="Fetch branches" onPress={fetchBranches} />
     </View>
   );
 }
